a0ae6cf82722737fb3ae6beecc0137840b88876c
magnouvean <c6af1d08-d039-472f-b890-6eaf2e96e961@anonaddy.me>
2023-04-08 14:23:27 +0200

    Fixed neural network implementation to be fully functional

--- nn.jl
+++ nn.jl
@@ -1,10 +1,17 @@
 using Random
+using LinearAlgebra
+
+# May be used as activation function
+sigmoid(x) = 1 ./ (1 .+ exp.(-x))
+sigmoid_der(x) = sigmoid(x) .* (1 .- sigmoid(x))
+
+# May be used as loss function
+mse(x, y) = sum((x .- y) .^ 2) ./ length(x)
+mse_der(x, y) = -2 .* (x .- y) ./ length(x)
 
 mutable struct NeuralNet
     α::Matrix{Float64}
-    β::Matrix{Float64}
-    α_bias::Vector{Float64}
-    β_bias::Vector{Float64}
+    β::Vector{Float64}
     activation_function::Function
     activation_function_deriv::Function
     loss_function::Function
@@ -15,107 +22,150 @@ mutable struct NeuralNet
     max_rand_weights::Float64
 end
 
-sigmoid(x) = 1 ./ (1 .+ exp.(-x))
-sigmoid_der(x) = sigmoid(x) .* (1 .- sigmoid(x))
-mse(x, y) = sum((x .- y) .^ 2) ./ length(x)
-mse_der(x, y) = -2 .* (x .- y) ./ length(x)
-
+"""
+Construct a neural network with num_nodes nodes, and max_rand_weights which is
+the maximum value a weight can be given when creating the neural net.
+"""
 function NeuralNet(num_nodes::Int=10, max_rand_weights::Float64=0.1)
-    NeuralNet(Matrix(undef, 0, 0),
+    NeuralNet(
         Matrix(undef, 0, 0),
-        Vector(undef, 0),
         Vector(undef, 0),
         sigmoid,
         sigmoid_der,
         mse,
         mse_der,
         identity,
-        identity,
+        (x -> 1),
         num_nodes,
-        max_rand_weights)
+        max_rand_weights
+    )
 end
 
+"""
+Calculates the Z-matrix, which is the activations of the neurons in the hidden
+layer.
+"""
 function calculate_Z(net::NeuralNet, X::Array)
-    α_T_X = transpose(net.α) * transpose(X)
-    α_bias_matrix = repeat(net.α_bias, 1, size(α_T_X, 2))
-    net.activation_function(α_bias_matrix + α_T_X)
+    Z = zeros(size(X, 1), net.hidden_layer_size)
+    for i in 1:size(Z, 1)
+        for j in 1:size(Z, 2)
+            Z[i, j] = net.activation_function(transpose(net.α[:, j]) * X[i, :])
+        end
+    end
+    Z
 end
 
+"""
+Calculates the T-matrix, which is the Z-matrix applied with the β-s.
+"""
 function calculate_T(net::NeuralNet, Z::Array)
-    β_T_Z = transpose(net.β) * Z
-    β_bias_matrix = repeat(net.β_bias, 1, size(β_T_Z, 2))
-    β_bias_matrix + β_T_Z
+    T = zeros(size(Z, 1))
+    for i in 1:length(T)
+        T[i] = net.β[1] .+ transpose(net.β[2:length(net.β)]) * Z[i, :]
+    end
+    T
 end
 
+"""
+Forward-propagation of a neural network given explanatory variables X.
+"""
 function forward(net::NeuralNet, X::Array)
     Z = calculate_Z(net, X)
     T = calculate_T(net, Z)
-    transpose(net.output_function(T))
+    net.output_function.(T)
 end
 
-function calculate_loss(net::NeuralNet, X::Array, y::Array)
+"""
+Alias for forward
+"""
+predict(net::NeuralNet, X::Array) = forward(net, X)
+
+"""
+Calculates the loss function of a net with explanatory variables X and response
+y, and a regularization parameter λ.
+"""
+function calculate_loss(net::NeuralNet, X::Array, y::Array, λ::Float64)
     ŷ = forward(net, X)
-    net.loss_function(ŷ, y)
+    net.loss_function(ŷ, y) + λ * (sum(net.α .^ 2) + sum(net.β .^ 2))
 end
 
-function train(net::NeuralNet, X::Array, y::Array, γ::Float64=0.01, epochs::Int64=100)
-    N = size(X, 1)
-    size(y, 1) == N || error("y must have the same number of rows as X")
-
-    p = size(X, 2)
-    k = size(y, 2)
-    M = net.hidden_layer_size
-
-    net.α = rand(Float64, (p, M)) * net.max_rand_weights
-    net.α_bias = rand(Float64, M) * net.max_rand_weights
-    net.β = rand(Float64, (M, k)) * net.max_rand_weights
-    net.β_bias = rand(Float64, k) * net.max_rand_weights
-
-    for i in 1:epochs
-        loss = calculate_loss(net, X, y)
-        println("Epoch $i: (loss $loss)")
-
-        α_grad = zeros(p, M)
-        α_bias_grad = zeros(M)
-        β_grad = zeros(M, k)
-        β_bias_grad = zeros(k)
-        ŷ = forward(net, X)
-        Z = calculate_Z(net, X)
-        δ = zeros(k, N)
-        s = zeros(M, N)
-        for i in 1:N
-            z_i = Z[:, i]
-            y_i = y[i, :]
-            ŷ_i = ŷ[i, :]
-            x_i = X[i, :]
-            for m in 1:M
-                for k_marked in 1:k
-                    loss_deriv = net.loss_function_deriv(y_i[k_marked], ŷ_i[k_marked])
-                    out_deriv = net.output_function_deriv(transpose(net.β[:, k_marked]) * z_i)
-                    δ[k, i] = loss_deriv * out_deriv
-                    β_grad[m, k_marked] += δ[k, i] * z_i[m]
-                end
-
-                s[m, i] = net.activation_function_deriv(transpose(net.α[:, m]) * x_i) * sum([net.β[m, k] * δ[k, i] for k in 1:k])
-                for l in 1:p
-                    α_grad[l, m] += s[m, i] * x_i[l]
-                end
+"""
+Calculates the gradient of the loss function with respect to α, for a given net,
+response variables y, predicted values ŷ, Z and T-matrices used when gathering
+the predictions and the regularization parameter λ.
+"""
+function calculate_β_grad(net::NeuralNet, y::Array, ŷ::Array, Z::Array, T::Array, λ::Float64)
+    β_grad = zeros(net.hidden_layer_size + 1)
+    for j in 0:length(β_grad)-1
+        for i in 1:size(y, 1)
+            if j == 0
+                β_grad[1] += -2 * (y[i, :] - ŷ[i, :]) ⋅ net.output_function_deriv.(T[i, :]) + 2 * λ * net.β[1]
+            else
+                β_grad[j+1] += -2 * (y[i, :] - ŷ[i, :]) ⋅ net.output_function_deriv.(T[i, :]) * Z[i, j] + 2 * λ * net.β[j+1]
             end
         end
-        net.α = net.α - γ * α_grad
-        net.β = net.β - γ * β_grad
+    end
+    β_grad
+end
+
+"""
+Calculates the gradient of the loss function with respect to α, for a given net,
+response variables y, predicted values ŷ, T-matrix used when gathering the
+predictions, the amount of explanatory variables X "p" and the regularization
+parameter λ.
+"""
+function calculate_α_grad(net::NeuralNet, y::Array, ŷ::Array, T::Array, p::Int64, λ::Float64)
+    α_grad = zeros(p, net.hidden_layer_size)
+    for h in 1:size(α_grad, 1)
+        for j in 1:size(α_grad, 2)
+            for i in 1:size(y, 1)
+                a = net.output_function_deriv(T[i, :]) * net.β[j] * net.activation_function_deriv(sum((net.α[h̃, j] * X[i, h̃] for h̃ in 1:p))) * X[i, h]
+                α_grad[h, j] += -2 * (y[i, :] - ŷ[i, :]) ⋅ a + 2 * λ * net.α[h, j]
+            end
+        end
+    end
+    α_grad
+end
+
+"""
+Train a neural network net with explanatory variables given by a matrix X and
+response vector y. Additionally one may specify a learning rate γ, the number of
+epochs and a regularization parameter (penalty term) λ.
+"""
+function train(net::NeuralNet, X::Array, y::Array, γ::Float64=0.01, epochs::Int64=100, λ::Float64=0.0)
+    size(y, 1) == size(X, 1) || error("y must have the same number of rows as X")
+
+    net.α = 2 * (rand(Float64, size(X, 2), net.hidden_layer_size) .- 0.5) * net.max_rand_weights
+    net.β = 2 * (rand(Float64, net.hidden_layer_size + 1) .- 0.5) * net.max_rand_weights
+
+    for epoch in 1:epochs
+        loss = calculate_loss(net, X, y)
+        println("Epoch $epoch: (loss $loss)")
+
+
+        Z = calculate_Z(net, X)
+        T = calculate_T(net, Z)
+        ŷ = forward(net, X) # Can be optimized
+
+        α_grad = calculate_α_grad(net, y, ŷ, T, size(X, 2), λ)
+        β_grad = calculate_β_grad(net, y, ŷ, Z, T, λ)
+
+        net.α -= γ * α_grad
+        net.β -= γ * β_grad
     end
 end
 
-function predict(net::NeuralNet, X::Array)
-    forward(net, X)
-end
 
-# Generate some somewhat arbitrary dataset
-X = [1.0 0.0; 1.0 1.0; 1.0 1.41; 0.0 2.0; 2.0 1.0; 2.0 1.41; 1.41 2.236; 2.0 2.0; 0.0 3.0; 3.0 1.0] / 3.0
-y = [1.0 0.1; 2.0 0.1; 3.0 0.1; 4.0 0.1; 5.0 0.1; 6.0 0.1; 7.0 0.1; 8.0 0.1; 9.0 0.1; 10.0 0.1] / 10.0
+# Demo for neural network
+using Plots
+Random.seed!(1234)
+include("data.jl")
+X /= maximum(X)
+X_0 = Vector(range(0.0, 1.0, length=101))
 
-# Train the network
-net = NeuralNet(20, 0.01)
-train(net, X, y, 0.01, 200)
-println("Predictions: $(10 * predict(net, X))")
+net = NeuralNet(10, 0.0001)
+train(net, X, y, 0.0001, 5000, 0.0)
+ŷ = predict(net, X_0)
+
+scatter(X, y, label="y")
+display(plot!(X_0, ŷ[:, 1], label="ŷ"))
